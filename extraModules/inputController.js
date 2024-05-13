dd5d8793b1217a00ffba523c87165d43746f5ead
DanuchiTheSorcerer <daniel.sande732@gmail.com>
2024-05-13 06:58:55 -0400

    Adds key inputs to the inputController class

--- extraModules/inputController.js
+++ extraModules/inputController.js
@@ -3,9 +3,12 @@ export class InputController {
         this.mouseX = 0;
         this.mouseY = 0;
         this.leftMouse = false;
+        this.keys = []
         document.addEventListener("mousemove", this.getMousePos.bind(this));
         document.addEventListener("mousedown", this.mouseDown.bind(this));
         document.addEventListener("mouseup", this.mouseUp.bind(this));
+        document.addEventListener("keydown", this.keyDown.bind(this));
+        document.addEventListener("keyup", this.keyUp.bind(this));
     }  
     mouseDown() {
         this.leftMouse = true
@@ -17,7 +20,14 @@ export class InputController {
         this.mouseX = e.clientX;
         this.mouseY = e.clientY;
     }
-
+    keyDown(e) {
+        if (!e.repeat) {
+            this.keys.push(e.code)
+        }
+    }
+    keyUp(e) {
+        this.keys = this.keys.filter(item => item !== e.code)
+    }
     getInputPacket() {
         let returnValue = {}
         //initiate vars for landscape
@@ -35,16 +45,21 @@ export class InputController {
             returnValue = {
                 mouseX: this.mouseX*(1200/innerWidth),
                 mouseY: (this.mouseY - sideLength)*(675/innerHeight),
-                leftMouse:this.leftMouse
+                leftMouse:this.leftMouse,
+                keys: this.keys
             }
         } else {
             returnValue = {
                 mouseX: (this.mouseX - sideLength)*(1200/innerWidth),
                 mouseY: this.mouseY*(675/innerHeight),
-                leftMouse:this.leftMouse
+                leftMouse:this.leftMouse,
+                keys: this.keys
             }
         }
-        document.getElementById("console").innerText = Math.floor(returnValue.mouseX) + " " + Math.floor(returnValue.mouseY) + " " + returnValue.leftMouse
+        let console = Math.floor(returnValue.mouseX) + " " + Math.floor(returnValue.mouseY) + " " + returnValue.leftMouse
+        console = ""
+        for (let i = 0;i<this.keys.length;i++) {console = console + this.keys[i] + " "}
+        document.getElementById("console").innerText = console
         return returnValue
     }
 }
